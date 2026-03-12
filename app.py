498dacd921c4b55da0de57536a1c9ea5c4a0d230
Guggilam Nagasri <nagasree0718@gmail.com>
2026-03-12 21:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import numpy as np
 import joblib
+
 # Load model and scaler
 model = joblib.load("diabetes_model.pkl")
 scaler = joblib.load("scaler.pkl")
+
 st.title("Diabetes Prediction System")
 st.write("Enter patient medical details")
+
 preg = st.number_input("Pregnancies", 0, 20)
 glucose = st.number_input("Glucose", 0, 200)
 bp = st.number_input("Blood Pressure", 0, 150)
@@ -14,10 +17,14 @@ insulin = st.number_input("Insulin", 0, 900)
 bmi = st.number_input("BMI", 0.0, 70.0)
 dpf = st.number_input("Diabetes Pedigree Function", 0.0, 3.0)
 age = st.number_input("Age", 1, 120)
+
 if st.button("Predict"):
+
     data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
     data = scaler.transform(data)
+
     prediction = model.predict(data)
+
     if prediction[0] == 1:
         st.error("Patient likely has Diabetes")
     else:
